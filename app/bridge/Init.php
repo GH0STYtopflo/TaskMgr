72a9253b64d3bb4c39f58721d7fe42b0c9d3f126
Sepehr <soapss85@proton.me>
2026-09-08 17:03:59 +0330

    ops: Revert changes to the init file

--- app/bridge/Init.php
+++ app/bridge/Init.php
@@ -13,7 +13,6 @@ use ghosty\taskmgr\database\Connection;
 use ghosty\taskmgr\database\DBHandle;
 use ghosty\taskmgr\models\CategoryModel;
 use ghosty\taskmgr\models\CommentModel;
-use ghosty\taskmgr\models\LogModel;
 use ghosty\taskmgr\models\SubTaskModel;
 use ghosty\taskmgr\models\TaskModel;
 use ghosty\taskmgr\models\UserModel;
@@ -30,12 +29,12 @@ class Init
         // Get connection to database (env read from dotenv)
         try {
             $conn = Connection::getConnection(
-                'pgsql',
-                'localhost',
-                '5432',
-                'taskmgr',
-                'postgres',
-                'postgres',
+                getenv('TMG_DBMS'),
+                getenv('TMG_HOST'),
+                getenv('TMG_PORT'),
+                getenv('POSTGRES_DB'),
+                getenv('POSTGRES_USER'),
+                getenv('POSTGRES_PASSWORD'),
                 [3 => 2, 19 => 2, 20 => false]
             );
         } catch (\Exception $e) {
@@ -51,17 +50,16 @@ class Init
         $subModel = new SubTaskModel($handle);
         $taskModel = new TaskModel($handle);
         $userModel = new UserModel($handle);
-        $logModel = new LogModel($handle);
 
         // JWT handle
-        $jwt = new JWT(200000, 'random_secret_that_will_be_read_from_dotenv_file_just_making_this_long_enough');
+        $jwt = new JWT(getenv('TMG_TOKEN_EXP'), getenv('TMG_SECRET'));
 
         // Create Services
-        $srvCategory = new CategoryService($catModel, $taskModel, $logModel);
-        $srvComment = new CommentService($comModel, $userModel, $taskModel, $logModel);
-        $srvSubtask = new SubTaskService($subModel, $taskModel, $logModel);
-        $srvTask = new TaskService($taskModel, $userModel, $catModel, $subModel, $logModel);
-        $srvUser = new UserService($userModel, $taskModel, $jwt, $handle, $logModel);
+        $srvCategory = new CategoryService($catModel, $taskModel);
+        $srvComment = new CommentService($comModel, $userModel, $taskModel);
+        $srvSubtask = new SubTaskService($subModel, $taskModel);
+        $srvTask = new TaskService($taskModel, $userModel, $catModel, $subModel);
+        $srvUser = new UserService($userModel, $taskModel, $jwt, $handle);
 
         // Feed these models to controllers
         $catCtl = new CategoryController($srvCategory);
